a49a494ee2ee83ed263b3ac0a2a8d159887346b4
JAlexCarney <JAlexCarney@gmail.com>
2022-04-26 19:49:57 -0500

    Initial commit

---
+++ Assets/_Scripts/BaseBurnable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseBurnable : MonoBehaviour
+{
+    public float size = 1;
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform cameraHolder;
+    private new Transform camera;
+    private float size = 10f;
+    private readonly float horizontalAcc = 0.5f;
+    private readonly float yeetForce = 10f;
+    private readonly float cameraRotationSpeed = 0.05f;
+    private Rigidbody rb;
+    private bool moving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        camera = cameraHolder.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (inputVector.magnitude > 0)
+        {
+            var adjustedInputVector = GlobalUtil.Rotate(inputVector, - (cameraHolder.eulerAngles.y));
+            rb.AddForce(new Vector3(adjustedInputVector.x, 0f, adjustedInputVector.y) * horizontalAcc);
+            transform.LookAt(new Vector3(transform.position.x + adjustedInputVector.x,
+                transform.position.y,
+                transform.position.z + adjustedInputVector.y));
+            moving = true;
+        }
+        else if (moving == true)
+        {
+            moving = false;
+            rb.velocity *= 0;
+            transform.LookAt(new Vector3(camera.position.x, transform.position.y, camera.position.z));
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            cameraHolder.eulerAngles = new Vector3(0f, cameraHolder.eulerAngles.y - cameraRotationSpeed, 0f);
+            transform.LookAt(new Vector3(camera.position.x, transform.position.y, camera.position.z));
+        }
+        else if (Input.GetKey(KeyCode.Q)) 
+        {
+            cameraHolder.eulerAngles = new Vector3(0f, cameraHolder.eulerAngles.y + cameraRotationSpeed, 0f);
+            transform.LookAt(new Vector3(camera.position.x, transform.position.y, camera.position.z));
+        }
+
+        if (Input.GetAxis("Jump") > 0)
+        {
+            rb.AddForce(new Vector3(0f, yeetForce, 0f));
+        }
+    }
+
+    private void Consume(BaseBurnable burnable) 
+    {
+        if (size > burnable.size) {
+            size += burnable.size/10f;
+            transform.localScale = Vector3.one * size / 10f;
+            Destroy(burnable.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Burnable")) 
+        {
+            Consume(collision.gameObject.GetComponent<BaseBurnable>());
+        }
+    }
+}
